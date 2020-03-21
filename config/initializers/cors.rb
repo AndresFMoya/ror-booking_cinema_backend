c4d9ea905378d6b5573522a62d8238d59a071745
andres <andresfmoya@gmail.com>
2020-03-21 12:32:29 -0500

    chore: add CD to Heroku

--- config/initializers/cors.rb
+++ config/initializers/cors.rb
@@ -7,7 +7,7 @@
 
 Rails.application.config.middleware.insert_before 0, Rack::Cors do
   allow do
-    origins 'http://localhost:3001'
+    origins 'https://movie-booking-front.herokuapp.com'
 
     resource '*',
              headers: :any,
